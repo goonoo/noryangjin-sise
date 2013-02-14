371fb669c3eac6651838235ddb56e5176267e922
tenshi <mctenshi@gmail.com>
2013-02-14 22:05:58 +0900

    킹크랩, 대게, 랍스터, 감성돔 등 최소거래수량 지정, 털게는 뺌.

--- test/test.js
+++ test/test.js
@@ -1,13 +1,31 @@
 /*global describe, it */
+
 "use strict";
 
 var assert = require("assert");
 var sise = require('../lib/sise');
+var config = require('../config.json');
 
 describe('sise', function () {
   it('getByConsonant', function (done) {
     sise.getByConsonant('20121224', 'ㄱ', function (list) {
       assert.equal(true, list.length > 0);
+      done();
+    });
+  });
+
+  it('get', function (done) {
+    var daege = config.fishes[1];
+    sise.get(daege, '20130214', function (price) {
+      assert.equal(15000, price);
+      done();
+    });
+  });
+
+  it('get - lessThanMin', function (done) {
+    var kingcrap = config.fishes[0];
+    sise.get(kingcrap, '20130214', function (price) {
+      assert.equal(null, price);
       done();
     });
   });
